aa577c66a63f24d8ef1f409d979a71780548ec49
Hanjae Jung <>
2022-10-28 21:24:46 +0900

    DB config 관련 처리를 struct 형태로 변경

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,12 @@
 use serde_derive::Deserialize;
 use std::fs::File;
 use std::io::prelude::*;
+use postgres::types::IsNull::No;
+
+pub struct ConfigLoader {
+    loaded: bool,
+    config: Option<Config>,
+}
 
 #[derive(Deserialize)]
 struct Config {
@@ -16,32 +22,58 @@ struct PostgresConfig {
     dbname: String,
 }
 
-fn get_toml_string(profile:&str) -> String {
-    let filename = format!("src/resources/{profile}.toml");
-    let mut toml_file = match File::open(&filename) {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == std::io::ErrorKind::NotFound => {
-            panic!("{} file not found", filename)
-        },
-        Err(error) => {
-            panic!("{} file open error {:?}", filename, error)
-        },
-    };
+impl ConfigLoader {
+    // private ===================================================================================================
+    fn get_toml_string(profile:&str) -> String {
+        let file_path = format!("src/resources/{profile}.toml");
+        let mut toml_file = match File::open(&file_path) {
+            Ok(file) => file,
+            Err(ref error) if error.kind() == std::io::ErrorKind::NotFound => {
+                panic!("{} file not found", file_path)
+            },
+            Err(error) => {
+                panic!("{} file open error {:?}", file_path, error)
+            },
+        };
 
-    let mut contents = String::from("");
-    toml_file.read_to_string(&mut contents).expect("toml file read error");
-    contents
-}
+        let mut contents = String::from("");
+        toml_file.read_to_string(&mut contents).expect("toml file read error");
+        contents
+    }
 
-pub fn get_db_connection_config(profile:&str) -> String {
-    let toml_string = get_toml_string(profile);
-    let config: Config = toml::from_str(toml_string.as_str()).unwrap();
+    // public ===================================================================================================
+    pub fn new() -> ConfigLoader {
+        ConfigLoader{
+            loaded: false,
+            config: None,
+        }
+    }
 
-    format!("host={} user={} password={} dbname={} port={}",
-            config.postgres_config.host,
-            config.postgres_config.user,
-            config.postgres_config.password,
-            config.postgres_config.dbname,
-            config.postgres_config.port.unwrap(),
-    )
+    pub fn load_config(&mut self, profile: &str) {
+        let toml_string = ConfigLoader::get_toml_string(profile);
+        let config: Config = match toml::from_str(toml_string.as_str()) {
+            Ok(str) => str,
+            Err(error) => {
+                panic!("load_config toml::from_str error : {:?}", error)
+            }
+        };
+        self.config = Option::from(config);
+        self.loaded = true;
+    }
+
+    pub fn get_db_connection_config(self) -> Result<String, ()> {
+        if !self.loaded {
+            return Err(())
+        }
+
+        let postgres_config= self.config.unwrap().postgres_config;
+
+        Ok(format!("host={} user={} password={} dbname={} port={}",
+                   postgres_config.host,
+                   postgres_config.user,
+                   postgres_config.password,
+                   postgres_config.dbname,
+                   postgres_config.port.unwrap(),
+        ))
+    }
 }
